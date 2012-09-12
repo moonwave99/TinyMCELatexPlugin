7d7defd7255fde4c3751d52bd2584be6d567d01d
Diego Caponera <diego.caponera@gmail.com>
2012-09-12 16:53:26 +0200

    gh-pages ready.

---
+++ README.md
@@ -0,0 +1,1 @@
+#Github Pages for TinyMCE Latex Plugin - A plugin to write formulae in TinyMCE through Google APIs.
---
+++ javascripts/tiny_mce/plugins/latex/js/dialog.js
@@ -0,0 +1,41 @@
+tinyMCEPopup.requireLangPack();
+
+var LatexDialog = {
+
+	init : function() {
+		if (code = tinymce.activeEditor.selection.getNode().alt){
+
+			document.forms[0].latex_code.innerHTML = code;
+
+		}
+	},
+
+	insert : function() {
+
+		var latexCode = document.forms[0].latex_code.value;
+		var img = '<img class="latex" src="' + LatexDialog.getSrc(latexCode.replace(/</g, "&lt;").replace(/>/g, "&gt;")) + '" alt="'+ latexCode +'"/>';
+
+		tinyMCEPopup.editor.execCommand('mceInsertContent', false, img);
+		tinyMCEPopup.close();
+	},
+
+	preview : function() {
+
+		var latexCode = document.forms[0].latex_code.value
+
+		if (document.forms[0].latex_code.value != ''){
+
+			document.getElementById('previewImg').src = LatexDialog.getSrc(latexCode);
+
+		}
+
+	},
+
+	getSrc : function(code){
+
+		return 'https://chart.googleapis.com/chart?cht=tx&chf=a,s,000000|bg,s,FFFFFF00&chl=' + encodeURIComponent(code);
+
+	}
+};
+
+tinyMCEPopup.onInit.add(LatexDialog.init, LatexDialog);
